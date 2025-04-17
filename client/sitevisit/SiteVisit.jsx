a43d930f9b03070a4374bdde795cf340fe673703
sibi <ssibi769@gmail.com>
2025-04-17 21:39:47 +0530

    Modified SiteVisit

--- client/sitevisit/SiteVisit.jsx
+++ client/sitevisit/SiteVisit.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const SiteVisit = () => {
-  return <div>
-  <h1>Site Visit</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Site Visit</h1>
+      <h2>Hello World</h2>
+    </div>
+  );
 };
 
 export default SiteVisit;
