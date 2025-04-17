d882f012e7ca538b906bbcb38b12e12be2c38c0b
Sibi <159809671+Sibi-05@users.noreply.github.com>
2025-04-17 21:36:25 +0530

    Update app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import SiteVisit from "./sitevisit/SiteVisit.jsx"
 
 const app = () => {
-  return <div>App</div>;
+  return <div>
+  <h1>App</h1>
+    <SiteVisit />
+  </div>;
 };
 
 export default app;
