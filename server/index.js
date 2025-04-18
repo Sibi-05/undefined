c96914a06514793370e5c06c7c84bd8c4dd08a53
Sibi <159809671+Sibi-05@users.noreply.github.com>
2025-04-18 21:35:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ console.log("Added");
 console.log(
   "Introducing ChatGPT ... We've trained a model called ChatGPT which interacts in a conversational way"
 );
-
+console.log("got it");
 console.log("Git Commit");
